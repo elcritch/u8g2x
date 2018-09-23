3fdf7e61bc194d4f37063aaa80ef161c6f5a77bc
Jaremy Creechley <creechley@gmail.com>
2018-09-23 01:44:53 -0600

    New project

---
+++ lib/u8g2x.ex
@@ -0,0 +1,18 @@
+defmodule U8g2x do
+  @moduledoc """
+  Documentation for U8g2x.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> U8g2x.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/u8g2x_test.exs
@@ -0,0 +1,8 @@
+defmodule U8g2xTest do
+  use ExUnit.Case
+  doctest U8g2x
+
+  test "greets the world" do
+    assert U8g2x.hello() == :world
+  end
+end
